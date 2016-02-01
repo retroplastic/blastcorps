3cdc80614ac3c2d519b5650dfbecbc7d69a38379
queueRAM <queueRAM@gmail.com>
2016-02-01 17:51:34 -0600

    Clean up formatting and fill unused code with nops

--- extend/hd_code_text.asm
+++ extend/hd_code_text.asm
@@ -5,22 +5,16 @@ include "N64.INC"
 
 origin 0x119BC
 
-  sll   t6, t6, 0x3    // each entry is two words
-  lui   at, 0xB080     // %hi(0xB07FA000), 0xB0000000 = ROM
-  addu  at, at, t6     //
-  lw    t6, 0xA000(at) // %lo(0xB07FA000)
-  sw    t6, 0x24(sp)   //
-  lw    t7, 0xA004(at) // %lo(0xB07FA004)
-  sub   t7, t7, t6     //
-  lw    t4, 0x30(sp)   //
-  sw    t7, 0x0(t4)    //
-  b     0x00012240     //
-  // these nops allow the new gzip code to be smaller than the original
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
+sll   t6, t6, 0x3    // each entry is two words
+lui   at, 0xB080     // %hi(0xB07FA000), 0xB0000000 = ROM
+addu  at, at, t6     //
+lw    t6, 0xA000(at) // %lo(0xB07FA000)
+sw    t6, 0x24(sp)   //
+lw    t7, 0xA004(at) // %lo(0xB07FA004)
+sub   t7, t7, t6     //
+lw    t4, 0x30(sp)   //
+sw    t7, 0x0(t4)    //
+b     0x12240        //
+
+// fill with nops to allow the new gzip code to be smaller than the original
+fill 0x12240-pc()
