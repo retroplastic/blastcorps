ea167700a8f4c774b4d20009d93092ade38af1d0
queueRAM <queueRAM@gmail.com>
2016-02-01 06:25:43 -0600

    Add bass assembler LUT implementation

--- extend/build.sh
+++ extend/build.sh
@@ -1,23 +1,29 @@
 #!/bin/sh
 set -x #echo on
 
+BASS=~/git/bass/bass/bass
+
+CODE=./bin/hd_code_text.raw
 ROM=bc.u.z64
 
 # copy clean ROM
-cp bc.u.z64.bak $ROM
+cp $ROM.bak $ROM
+cp $CODE.bak $CODE
 
-# apply patch
-# TODO: apply asm patch?
+# apply LUT patch
 dd if=lut.bin of=$ROM conv=notrunc bs=1 seek=$((0x7FA000))
 
+# apply source patch
+$BASS -o $CODE use_lut.s
+
 # gzip code block
-gzip -c bin/hd_code_text.raw > bin/hd_code_text.raw.gz
+gzip -c $CODE > $CODE.gz
 
 # TODO: assert new gzip size is <= old gzip
 
 # overwrite code
 dd if=/dev/zero of=$ROM conv=notrunc bs=1 seek=$((0x787FD0)) count=$((0x7D73B4-0x787FD0))
-dd if=bin/hd_code_text.raw.gz of=$ROM conv=notrunc bs=1 seek=$((0x787FD0))
+dd if=$CODE.gz of=$ROM conv=notrunc bs=1 seek=$((0x787FD0))
 
 # update checksum
 ../../sm64tools/n64cksum $ROM
---
+++ extend/use_lut.s
@@ -0,0 +1,24 @@
+arch n64.cpu
+endian msb
+
+include "N64.INC"
+
+origin 0x119BC
+
+  sll   t6, t6, 0x3    //
+  lui   at, 0x80       // %hi(0x7FA000)
+  addu  at, at, t6     //
+  lw    t6, 0xA000(at) // %lo(0x7FA000)
+  sw    t6, 0x24(sp)   //
+  lw    t7, 0xA004(at) // %lo(0x7FA004)
+  sub   t7, t7, t6     //
+  lw    t4, 0x30(sp)   //
+  sw    t7, 0x0(t4)    //
+  b     0x00012240     //
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
