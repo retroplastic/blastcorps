b154a69c5aab2b5697558496b08b5a92855ce960
queueRAM <queueRAM@gmail.com>
2016-07-28 23:11:13 -0500

    Add constants for (E), (J) ROMs and use pseudo-instructions

--- extend/hd_code_text.asm
+++ extend/hd_code_text.asm
@@ -1,15 +1,26 @@
-// Blast Corps (U) (V1.1) patch to hd_code_text.raw to use ROM lookup table
+// Blast Corps/Dozer patch to hd_code_text.raw to use ROM lookup table
 // Updated: 2016 Jul 28
 // By: queueRAM
 // For updates, check: http://origami64.net/showthread.php?tid=334&pid=2566
 
 // Use with N64 version of bass: https://github.com/ARM9/bass
-// > bass -o hd_code_text.raw hd_code_text.asm
+// > bass -d VERSION=VER_U0 -o hd_code_text.raw hd_code_text.asm
+// supply define for VERSION, one of: VER_J, VER_U0, VER_U1, VER_E
 
 arch n64.cpu
 endian msb
 
 include "mips4300.inc"
+
+// seek To RAM address
+macro seek(variable offset) {
+  if (offset >= 0x802447C0 && offset < 0x802E8BD0) {
+     origin (offset - 0x802447C0)
+  } else {
+     error "Bad seek()"
+  }
+  base offset
+}
 
 //-----------
 // OS defines
@@ -42,46 +53,83 @@ constant VER_U1(2)
 constant VER_E(3)
 
 if !{defined VERSION} {
-  error "Must define VERSION: U1.0, U1.1, J"
+  error "Must define VERSION: VER_J, VER_U0, VER_U1, VER_E"
 }
 
 //-----------
 // (U) (V1.0)
 //-----------
 if {VERSION} == VER_U0 {
+  constant LoadLevelBase(0x8025615C)
+  constant LoadLevelEnd(0x80256A34)
+
   constant osInvalDCache(0x802D6690)
   constant osPiStartDma(0x802DA240)
   constant osRecvMesg(0x802D4860)
 
   constant InitiateDma(0x8028B4C4)
 
-  constant JUMP_TABLE_BASE(0x80308AF0)
+  constant JUMP_TABLE_ADDR(0x80308AF0)
+
+  constant MESSAGE_BLOCK_ADDR(0x80370BA8)
+  constant MESSAGE_QUEUE_ADDR(0x80314FF0)
 }
 
 //-----------
 // (U) (V1.1)
 //-----------
 if {VERSION} == VER_U1 {
+  constant LoadLevelBase(0x8025615C)
+  constant LoadLevelEnd(0x80256A34)
+
   constant osInvalDCache(0x802D6740)
   constant osPiStartDma(0x802DA2F0)
   constant osRecvMesg(0x802D4910)
 
   constant InitiateDma(0x8028B4C4)
 
-  constant JUMP_TABLE_BASE(0x80308BA0)
+  constant JUMP_TABLE_ADDR(0x80308BA0)
+
+  constant MESSAGE_BLOCK_ADDR(0x80370C58)
+  constant MESSAGE_QUEUE_ADDR(0x803150A0)
 }
 
 //-----
 // (J)
 //-----
 if {VERSION} == VER_J {
+  constant LoadLevelBase(0x8025615C)
+  constant LoadLevelEnd(0x80256A34)
+
   constant osInvalDCache(0x802D6B10)
   constant osPiStartDma(0x802DA6C0)
   constant osRecvMesg(0x802D4CE0)
 
   constant InitiateDma(0x8028B824)
 
-  constant JUMP_TABLE_BASE(0x80308E30)
+  constant JUMP_TABLE_ADDR(0x80308E30)
+
+  constant MESSAGE_BLOCK_ADDR(0x80370D38)
+  constant MESSAGE_QUEUE_ADDR(0x80315160)
+}
+
+//-----
+// (E)
+//-----
+if {VERSION} == VER_E {
+  constant LoadLevelBase(0x80257594)
+  constant LoadLevelEnd(0x80257E6C)
+
+  constant osInvalDCache(0x802D91C0)
+  constant osPiStartDma(0x802DCC70)
+  constant osRecvMesg(0x802D7280)
+
+  constant InitiateDma(0x8028D1E4)
+
+  constant JUMP_TABLE_ADDR(0x803085C0)
+
+  constant MESSAGE_BLOCK_ADDR(0x80376618)
+  constant MESSAGE_QUEUE_ADDR(0x803174F8)
 }
 
 //---------
@@ -89,8 +137,7 @@ if {VERSION} == VER_J {
 //---------
 
 // hd_code_text.raw offset 0x1199C is RAM 0x8025615C
-origin 0x1199C
-base 0x8025615C
+seek(LoadLevelBase)
 LoadLevel:
   // begin no changes
   addiu sp, sp, -0x28
@@ -108,45 +155,38 @@ LoadLevel:
   // (U) (V1.0): 0x80308AE8
   // (U) (V1.1): 0x80308B98
   // (J):        0x80308E28
-  // (E):
+  // (E):        0x803085C0
   // 80308B98 contains the old jump table - use it to DMA table entries
 
-  // osInvalDCache(vAddr = 0x80308BA0, numBytes = 0x10)
-  lui   a0, JUMP_TABLE_BASE >> 16
-  ori   a0, JUMP_TABLE_BASE & 0xFFFF
+  // osInvalDCache(vAddr = JUMP_TABLE_ADDR, numBytes = 0x10)
+  li    a0, JUMP_TABLE_ADDR
   jal   osInvalDCache
   addiu a1, zero, 0x10
 
-  // osPiStartDma(0x80370C58, OS_MESG_PRI_NORMAL, OS_READ, devAddr, JUMP_TABLE_BASE, 0x10, 0x803150A0);
+  // osPiStartDma(MESSAGE_BLOCK_ADDR, OS_MESG_PRI_NORMAL, OS_READ, devAddr, JUMP_TABLE_BASE, 0x10, MESSAGE_QUEUE_ADDR);
   lw    t0, 0x28(sp) // load level id (a0)
   sll   t0, t0, 0x4  // *= 0x10
-  lui   a3, 0x7F
-  ori   a3, a3, 0xC000
+  li    a3, 0x7FC000
   addu  a3, a3, t0 // devAddr = 0x7FC000 + levelId * 0x10
-  lui   t1, JUMP_TABLE_BASE >> 16
-  ori   t1, t1, JUMP_TABLE_BASE & 0xFFFF
+  li    t1, JUMP_TABLE_ADDR
   sw    t1, 0x10(sp)
   addiu t2, zero, 0x10
   sw    t2, 0x14(sp)
-  lui   t3, 0x8031
-  ori   t3, 0x50A0
+  li    t3, MESSAGE_QUEUE_ADDR
   sw    t3, 0x18(sp)
-  lui   a0, 0x8037
-  ori   a0, a0, 0x0C58
+  li    a0, MESSAGE_BLOCK_ADDR
   addiu a2, zero, OS_READ
   jal   osPiStartDma
   addiu a1, zero, OS_MESG_PRI_NORMAL
 
-  // osRecvMesg(0x803150A0, NULL, OS_MESG_BLOCK);
-  lui   a0, 0x8031
-  ori   a0, a0, 0x50A0
+  // osRecvMesg(MESSAGE_QUEUE_ADDR, NULL, OS_MESG_BLOCK);
+  li    a0, MESSAGE_QUEUE_ADDR
   addiu a1, zero, 0x0
   jal   osRecvMesg
   addiu a2, zero, OS_MESG_BLOCK
 
   // save base ROM address and length
-  lui   at, JUMP_TABLE_BASE >> 16
-  ori   at, at, JUMP_TABLE_BASE & 0xFFFF
+  li    at, JUMP_TABLE_ADDR
   lw    t6, 0x0(at)
   sw    t6, 0x24(sp)   // store base address
   lw    t7, 0xC(at)
@@ -172,6 +212,6 @@ LoadLevel_Dma:
   // end no changes
 
 // fill existing code with nops to allow the new gzip code to be smaller than the original
-fill 0x80256A34-pc()
+fill LoadLevelEnd - pc()
 
 // end LoadLevel
