400f7b8736d0babe72219900a7ccd595addc00e0
retroplastic <retroplastic@protonmail.com>
2021-12-25 14:29:15 +0100

    tools: Use local std_image.

--- tools/src/n64graphics.c
+++ tools/src/n64graphics.c
@@ -5,9 +5,9 @@
 #define STBI_NO_HDR
 #define STBI_NO_TGA
 #define STB_IMAGE_IMPLEMENTATION
-#include <stb/stb_image.h>
+#include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include <stb/stb_image_write.h>
+#include "stb_image_write.h"
 
 #include "n64graphics.h"
 #include "utils.h"
