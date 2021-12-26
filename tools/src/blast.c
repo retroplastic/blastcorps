faf2710870d12e413197c22d62503b4564a9baee
retroplastic <retroplastic@protonmail.com>
2021-12-26 22:51:08 +0100

    tools/blast: Add support for blast4 and 5 / LUTs.

--- tools/src/blast.c
+++ tools/src/blast.c
@@ -722,14 +722,18 @@ void
 print_usage()
 {
   printf("Usage:\n");
+  printf("Unpack whole rom:\n");
   printf("./blast <rom_path>\n");
+  printf("Decode blast encoded file:\n");
   printf("./blast <file_path> <blast_compression_id>\n");
+  printf("Decode blast encoded file. With LUT for blast 4 and 5:\n");
+  printf("./blast <file_path> <blast_compression_id> <lut_file_path>\n");
 }
 
 int
 main(int argc, char* argv[])
 {
-  if (argc < 2 || argc > 3)
+  if (argc < 2 || argc > 4)
   {
     print_usage();
     return EXIT_FAILURE;
@@ -744,16 +748,41 @@ main(int argc, char* argv[])
     // decompress whole rom
     decompress_rom(argv[1], rom_bytes, size);
   }
-  else if (argc == 3)
+  else if (argc > 2)
   {
-    // convert single file
-    // convert_to_png(argv[1], size, atoi(argv[2]));
     blast_t type = atoi(argv[2]);
     printf("Opening %s\n", argv[1]);
 
+    if (argc != 4 && (type == BLAST4_IA16 || type == BLAST5_RGBA32))
+    {
+      printf(
+        "ERROR: You need to provide a LUT file for blast4 and blast5 files.\n");
+      print_usage();
+      return EXIT_FAILURE;
+    }
+
+    uint8_t* lut_bytes = NULL;
+    if (argc == 4)
+    {
+      printf("Opening LUT at %s\n", argv[3]);
+      size_t lut_size = read_file(argv[3], &lut_bytes);
+      if (type == BLAST4_IA16 && lut_size != 128)
+      {
+        printf(
+          "WARNING: Lookup table for blast4 is %ld bytes instead of 128.\n",
+          lut_size);
+      }
+      else if (type == BLAST5_RGBA32 && lut_size != 256)
+      {
+        printf(
+          "WARNING: Lookup table for blast5 is %ld bytes instead of 256.\n",
+          lut_size);
+      }
+    }
+
     uint8_t* decompressed_bytes = malloc(100 * size);
     int32_t decompressed_size =
-      decompress_block(rom_bytes, size, type, decompressed_bytes, NULL);
+      decompress_block(rom_bytes, size, type, decompressed_bytes, lut_bytes);
 
     // Write decompressed file
     char out_path_decompressed[512];
