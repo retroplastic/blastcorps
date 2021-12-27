4950891ea65a4e7694544baa8a950d708701ad74
retroplastic <retroplastic@protonmail.com>
2021-12-27 02:10:03 +0100

    tools/gen_splat: Use utils.c.

--- tools/src/gen_splat_yaml.c
+++ tools/src/gen_splat_yaml.c
@@ -7,46 +7,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int
-main(int argc, char** argv)
+#include "utils.h"
+
+void
+list_gzips(uint8_t* bytes, size_t size)
 {
-  if (argc != 2)
-  {
-    printf("Usage:\n");
-    printf("%s <rom_path>\n", argv[0]);
-    return EXIT_FAILURE;
-  }
-
-  FILE* rom = fopen(argv[1], "rb");
-  if (rom == NULL)
-  {
-    printf("Invalid ROM\n");
-    return EXIT_FAILURE;
-  }
-
-  fseek(rom, 0, SEEK_END);
-  size_t size = ftell(rom);
-  rewind(rom);
-
-  uint8_t* bytes = calloc(size, sizeof(uint8_t));
-  fread(bytes, sizeof(uint8_t), size, rom);
-
-  if (ferror(rom))
-  {
-    perror("Error reading rom file.");
-    return EXIT_FAILURE;
-  }
-
-  fclose(rom);
-
   for (uint64_t address = 0x0; address < size; address++)
   {
-// clang-format off
+    // clang-format off
     if (bytes[address]     == 0x1F &&
         bytes[address + 1] == 0x8B &&
         bytes[address + 2] == 0x08 &&
         bytes[address + 3] == 0x08)
-// clang-format on
+    // clang-format on
     {
       char name[300];
 
@@ -58,6 +31,22 @@ main(int argc, char** argv)
       printf("  - [0x%06lX, bin, %s]\n", address, name);
     }
   }
+}
+
+int
+main(int argc, char** argv)
+{
+  if (argc != 2)
+  {
+    printf("Usage:\n");
+    printf("%s <rom_path>\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  uint8_t* bytes;
+  size_t size = read_file(argv[1], &bytes);
+
+  list_gzips(bytes, size);
 
   free(bytes);
 
